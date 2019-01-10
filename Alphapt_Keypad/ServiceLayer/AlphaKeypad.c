87adda3c7d1af1a8686542cd71c1680c648f40f6
root <root@embeddedfab-Satellite-A135>
2019-01-10 12:44:07 +0200

    intial version to Enter Special chars.

--- Alphapt_Keypad/ServiceLayer/AlphaKeypad.c
+++ Alphapt_Keypad/ServiceLayer/AlphaKeypad.c
@@ -8,33 +8,38 @@
 #include "AlphaKeypad.h"
 
 
-extern char movecursur ;
+#define SPECHIAL_CHARNUM    10
+
+extern char movecursur;
+
+static   char SpecialCharFlag  =  0;
+
+static char alphaptic_number[10][4] =
+{
+		{' ','0','0','0'},
+		{'a','b','c','1'},
+		{'d','e','f','2'},
+		{'j','h','i','3'},
+		{'g','k','l','4'},
+		{'m','n','o','5'},
+		{'p','q','r','6'},
+		{'s','t','u','7'},
+		{'v','w','x','8'},
+		{'y','z','.','9'},
+
+};
+
+static char special_char[] = {'!','@','#','$' ,'%','^','&','*','-','_'};
 
 
-/*  " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"  */
-
-static char alphaptic_number[10][4] = {
-
-									  {' ','0','0','0'},
-									  {'a','b','c','1'},
-									  {'d','e','f','2'},
-									  {'j','h','i','3'},
-									  {'g','k','l','4'},
-									  {'m','n','0','5'},
-									  {'p','q','r','6'},
-									  {'s','t','u','7'},
-									  {'v','w','x','8'},
-									  {'y','z','.','9'},
-
-								     };
 
 char EF_Keypad_AlphpRead()
 {
-	volatile int  keyPresd       = -1;
-	unsigned char presCount      =  0;
-	signed 	 char alphmap   	 = -1;
-	signed   char last_Keypresed = -1;
-	static   char CaptlFlag      =  0;
+	volatile int  keyPresd         = -1;
+	unsigned char presCount        =  0;
+	signed 	 char alphmap   	   = -1;
+	signed   char last_Keypresed   = -1;
+	static   char CaptlFlag        =  0;
 
 
 	keyPresd = EF_KeypadRead();
@@ -47,12 +52,27 @@ char EF_Keypad_AlphpRead()
 			if(!(keyPresd == -1))
 			{
 
-
-
-				if((keyPresd !=  last_Keypresed) || (presCount == 4)   )
+				if((keyPresd !=  last_Keypresed) || (presCount == 4) || (presCount == SPECHIAL_CHARNUM)   )
 				{
-					presCount = 0;
-					EF_void_UART_SendString("reset.........\n");
+
+					if(SpecialCharFlag == TRUE)
+					{
+						if(presCount == SPECHIAL_CHARNUM)
+						{
+							presCount = 0;
+							EF_void_UART_SendString("reset special char.........\n");
+
+						}
+
+
+					}
+					else
+					{
+						presCount = 0;
+						EF_void_UART_SendString("reset.........\n");
+
+					}
+
 
 				}
 
@@ -69,12 +89,13 @@ char EF_Keypad_AlphpRead()
 				if((int)keyPresd == BUTTON_F4 )
 				{
 					CaptlFlag ^= 1;
-					EF_void_UART_SendString("Capitacl Flag\n");
+					SpecialCharFlag = FALSE;
+					EF_void_UART_SendString("Capital Flag\n");
 					break;
 				}
 
 				/* clear lcd */
-				if((int)keyPresd == BUTTON_F3)
+				if((int)keyPresd == BUTTON_F2)
 				{
 
 					movecursur = 1;
@@ -85,16 +106,40 @@ char EF_Keypad_AlphpRead()
 					break;
 				}
 
-
-				alphmap = Alphaptic_Map(keyPresd,presCount);
-				/* check capitalization   */
-				if(CaptlFlag == 1)
+				if((int)keyPresd == BUTTON_F3)
 				{
-					alphmap = lowerCharTO_UperCase(alphmap);
+					SpecialCharFlag ^= 1;
+					EF_void_UART_SendString("Spatial Char \n");
+					break;
 				}
 
-				EF_void_LCD_goto(1,movecursur);
-				EF_void_LCD_send_data((alphmap));
+
+				if(SpecialCharFlag == 1)
+				{
+
+					if(keyPresd == 1)
+					{
+						EF_void_LCD_goto(1,movecursur);
+						alphmap = special_char[presCount];
+						EF_void_LCD_send_data(alphmap);
+					}
+
+				}
+				else
+				{
+					alphmap = Alphaptic_Map(keyPresd, presCount);
+					/* check capitalization   */
+					if(CaptlFlag == 1)
+					{
+						alphmap = lowerCharTO_UperCase(alphmap);
+					}
+
+					EF_void_LCD_goto(1,movecursur);
+					EF_void_LCD_send_data((alphmap));
+
+				}
+
+
 
 				EF_void_TimerReset(WAIT_KEYCHAR_SCAN_ID);
 				last_Keypresed = (char)keyPresd;
@@ -116,11 +161,6 @@ char EF_Keypad_AlphpRead()
 	}
 
 
-//	EF_void_UART_SendString("Exit while\n");
-//	EF_void_UART_PutChar(alphmap);
-//	EF_void_UART_SendString("\n");
-
-
 	return alphmap;
 
 }
@@ -129,14 +169,16 @@ char EF_Keypad_AlphpRead()
 
 char Alphaptic_Map( char keyPressed,char pressedCount)
 {
-    if(  (keyPressed >=0) && (keyPressed <= 9) )
-    {
-    	return alphaptic_number[keyPressed][pressedCount];
+	if(  (keyPressed >=0) && (keyPressed <= 9) )
+	{
+		return alphaptic_number[keyPressed][pressedCount];
 
-    }
-    else{
-    	return '0';
-    }
+	}
+	else{
+
+		// TODO check this return later
+		return '0';
+	}
 }
 
 
@@ -154,7 +196,7 @@ char lowerCharTO_UperCase(char lower )
 	}
 
 
-	EF_void_UART_SendString("The uppercase equivalent is ");
+	EF_void_UART_SendString("The upper case equivalent is ");
 	EF_void_UART_PutChar(upper);
 	EF_void_UART_SendString("\n");
 
@@ -163,5 +205,36 @@ char lowerCharTO_UperCase(char lower )
 }
 
 
+
+char getMaxNum_ToRestPresCount(char keypessed)
+{
+
+	// if special flag true ----> SPECHIAL_CHARNUM
+	// if special flag False ---> and key number NOT equal ---> 4
+	// if special flag False ---> and key number  equal    ---> 3
+
+	if (SpecialCharFlag == 1)
+	{
+		return SPECHIAL_CHARNUM;
+	}
+	else
+	{
+		if(keypessed == 1)
+		{
+			return 3;
+		}
+		else
+		{
+			return 4;
+		}
+
+	}
+
+
+
+
+
+
+}
 
 
